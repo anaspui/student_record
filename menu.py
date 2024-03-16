e1d690f0953d83ce1f2fd3920c943303b1ec4364
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2024-03-15 19:28:19 -0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,25 @@
+def print_menu():
+    print("\n===== Student Record Manager =====")
+    print("1.\t Add Student")
+    print("2.\t Delete Student")
+    print("3.\t Update Student")
+    print("4.\t Display Student Details")
+    print("5.\t Sort Student")
+    print("6.\t See All Students")
+    print("7.\t Mark Student")
+    print("8.\t Search Student")
+    print("9.\t Export records")
+    print("10.\t Exit")
+
+
+def update_menu():
+    print("\n===== Update Student Record =====")
+    print("1.\t Name")
+    print("2.\t Email")
+    print("3.\t Department")
+    print("4.\t DOB")
+    print("5.\t CGPA")
+    print("6.\t Address")
+    print("7.\t Phone Number")
+    print("8.\t Marked Status")
+    print("9.\t Exit")
